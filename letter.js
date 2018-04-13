061f2605e070d306db7160c0082daf110dd54b0f
Yewbdar <Yewbdar.grima@gmail.com>
2018-04-12 23:35:06 -0500

    all files updated

--- letter.js
+++ letter.js
@@ -1,28 +1,17 @@
-/**
- * This is a Constant that is used in letter , 
- * This is needed as we can customize placeholders as per our need. 
- */
 
-// placeHolder = require("./PlaceHolder");
-// module.exports = {    
     var underScore = "_"; 
-    // dash : "-", 
-// }
 module.exports = function(character,guessed){
     this.character=character,
     this.guessed=guessed,
     this.isGuessed=function(char)
     {
-        
         if(this.guessed){
             return char;
             // console.log("am in here isGuessed****************")
         }else{
             return underScore;
-            // console.log("am in here isGuessed****************")
+            
         }
-         
-        
     },
     this.guessCharacter = function(charFromWord){
         
@@ -31,8 +20,6 @@ module.exports = function(character,guessed){
         } else{
             this.guessed = false
         }  
-        // console.log(this.guessed)
+        
     }
 };
-// var letter =new Letter("h",false);
-//   letter.guessCharacter("o");  
